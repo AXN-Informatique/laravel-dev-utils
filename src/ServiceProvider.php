1d55e40f3f9b87c7ee76140d50a18b2462065eed
Vincent Garnier <forxer@gmail.com>
2017-01-31 23:00:10 +0100

    add tinker

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -17,6 +17,7 @@ class ServiceProvider extends BaseServiceProvider
         \Axn\ModelsGenerator\ServiceProvider::class,
         \Barryvdh\Debugbar\ServiceProvider::class,
         \Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        \Laravel\Tinker\TinkerServiceProvider::class,
     ];
 
     /**
